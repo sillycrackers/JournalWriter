6f5efd32680ec7c27a63a85b80aa49e212dd5aa1
sillycrackers <westervelderik@gmail.com>
2021-11-23 23:17:09 -0500

    new CurrentUserElement Class

---
+++ JournalWriter/Models/CurrentUserElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace JournalWriter.Models
+{
+    public class CurrentUserElement : IDrawable
+    {
+        public void Draw()
+        {
+            
+        }
+    }
+}
