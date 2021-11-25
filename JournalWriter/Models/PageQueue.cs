4894570d843433c135cc79b485c55f70b3f05374
sillycrackers <westervelderik@gmail.com>
2021-11-25 11:30:19 -0500

    Adding Page Queue Class

---
+++ JournalWriter/Models/PageQueue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JournalWriter.Views;
+
+namespace JournalWriter.Models
+{
+    public class PageQueue
+    {
+        private List<Page> stack = new List<Page>();
+
+        public void Push(Page p)
+        {
+            stack.Add(p);
+        }
+        public Page Pop()
+        {
+            return stack[stack.Count - 1];
+        }
+        public Page GetLastPage()
+        {
+            return stack[stack.Count - 1];
+        }
+    }
+}
