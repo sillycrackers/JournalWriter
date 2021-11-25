b4fb5e091dd26cd7a749de87e3564a95d4a10934
sillycrackers <westervelderik@gmail.com>
2021-11-25 15:22:33 -0500

    Adding stucts for page names

---
+++ JournalWriter/Structs/MenuNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JournalWriter.Structs
+{
+    public struct MenuNames
+    {
+        public string MainMenu { get; set; }
+        public string LoginMenu { get; set; }
+        public string PastEntriesMenu { get; set; }
+
+    }
+}
---
+++ JournalWriter/Structs/PageNames.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JournalWriter.Structs
+{
+    public struct PageNames
+    {
+        public string MainPage { get; set; }
+
+        public string LoginPage { get; set; }
+
+        public string PastEntriesPage { get; set; }
+    }
+}
